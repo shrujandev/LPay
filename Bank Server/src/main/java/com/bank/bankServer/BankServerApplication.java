0bc29fbdbc0d1a3bc552e616f6150fcfebda8e25
shrujandev <shrujandev1824@gmail.com>
2023-04-16 07:30:33 +0530

    Bank server dependency update

---
+++ Bank Server/src/main/java/com/bank/bankServer/BankServerApplication.java
@@ -0,0 +1,13 @@
+package com.bank.bankServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankServerApplication.class, args);
+	}
+
+}
---
+++ Bank Server/src/test/java/com/bank/bankServer/BankServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.bankServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
